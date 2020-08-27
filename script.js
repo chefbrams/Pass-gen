0843a315b15f1fcfac72cef73c82d4bed3368b05
Chef Brams <oldcitycatering@gmail.com>
2020-08-27 15:49:59 -0500

    final loop

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function writePassword() {
 
 var randArr;
 
+
 function generatePassword() {
   var passLength = prompt("How many characters would you like your password between 8 and 128?");
   console.log(passLength);
@@ -20,11 +21,22 @@ function generatePassword() {
     alert("The password is too short/long; please try again!");
     var passLength = prompt("How many characters would you like your password between 8 and 128?");
   }
+
   var passNumber = confirm("Would you like numbers in your password?");
   console.log(passNumber)
   var passUpper = confirm("Would you like Upper Case characters in your password?");
   var passLower = confirm("Would you like lower case characters in your password?");
   var passSpecial = confirm("Would you like special characters in your password?");
+
+  // if nothing is confirmed
+  while (passNumber === false && passSpecial === false && passUpper === false && passLower === false) {
+    alert("One option must be chosen.  Please pick again!")
+    var passNumber = confirm("Would you like numbers in your password?");
+    console.log(passNumber)
+    var passUpper = confirm("Would you like Upper Case characters in your password?");
+    var passLower = confirm("Would you like lower case characters in your password?");
+    var passSpecial = confirm("Would you like special characters in your password?");
+  }
 
   var arrNumber = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   var arrUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -36,24 +48,77 @@ function generatePassword() {
     console.log(randArr);
   }
 
-  else if (passLower) {
+  if (passLower) {
     randArr = arrLower;
     console.log(randArr);
   }
+  if (passSpecial) {
+    randArr = arrChar;
+    console.log(randArr);
+  }
+  if (passNumber) {
+    randArr = arrNumber;
+    console.log(randArr);
+  }
 
+  if (passLower && passUpper) {
+    randArr = arrLower.concat(arrUpper);
+    console.log(randArr);
+  }
+  if (passLower && passSpecial) {
+    randArr = arrLower.concat(arrChar);
+    console.log(randArr);
+  }
+  if (passLower && passNumber) {
+    randArr = arrLower.concat(arrNumber);
+    console.log(randArr);
+  }
+  if (passUpper && passNumber) {
+    randArr = arrUpper.concat(arrNumber);
+    console.log(randArr);
+  }
+  if (passUpper && passSpecial) {
+    randArr = arrUpper.concat(arrChar);
+    console.log(randArr);
+  }
+  if (passSpecial && passNumber) {
+    randArr = arrChar.concat(arrNumber);
+    console.log(randArr);
+  }
+  if (passLower && passUpper && passSpecial) {
+    randArr = arrLower.concat(arrUpper, arrChar);
+    console.log(randArr);
+  }
+  if (passNumber && passUpper && passSpecial) {
+    randArr = arrNumber.concat(arrUpper, arrChar);
+    console.log(randArr);
+  }
+  if (passLower && passNumber && passSpecial) {
+    randArr = arrLower.concat(arrNumber, arrChar);
+    console.log(randArr);
+  }
+  if (passLower && passUpper && passNumber) {
+    randArr = arrLower.concat(arrUpper, arrNumber);
+    console.log(randArr);
+  }
+  if (passLower && passUpper && passSpecial && passNumber) {
+    randArr = arrLower.concat(arrUpper, arrChar, arrNumber);
+    console.log(randArr);
+  }
 
+  for (var i = 0; i < passLength; i++) {
+    var randPass = randArr[Math.floor(Math.random() * randArr.length)];
+    // inside loop concat the random char to randPass
 
+  }
 
-  //use for loop to pick a random char from available options 
-
-  // inside loop concat the random char to randPass
+  return randPass
 
 
 
 
   // outside loop lastly just return randPass 
 }
-
 
 
 // Add event listener to generate button
