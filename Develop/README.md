52c785392f5a2c4fd2204d46cd2c4b1b33d8e8c6
Chef Brams <oldcitycatering@gmail.com>
2020-08-28 08:34:51 -0500

    final push before submission

---
+++ Develop/README.md
@@ -0,0 +1,14 @@
+
+# 03 JavaScript: Password Generator
+
+This is an application that generates a random password based on user-selected criteria. This app will run in the browser and features dynamically updated HTML and CSS powered by JavaScript. It also features a clean and polished user interface that is dynamic and responsive, ensuring that it adapts and fits on multiple screen sizes.
+
+![password generator demo](./Assets/03-javascript-homework-demo.png)
+
+## Try it for yourself! Click link below!
+
+ [Password Generator](https://chefbrams.github.io/Pass-gen/)
+
+
+
+
--- Develop/script.js
+++ Develop/script.js
@@ -14,17 +14,17 @@ var randArr;
 
 
 function generatePassword() {
-  var passLength = parseInt(prompt("How many characters would you like your password between 8 and 128?"));
+  var passLength = parseInt(prompt("Please choose between 8 and 128 characters for your password"));
   console.log(passLength);
   // loops until conditions are met
   while (passLength < 8 || passLength > 128) {
-    alert("The password is too short/long; please try again!");
-    var passLength = parseInt(prompt("How many characters would you like your password between 8 and 128?"));
+    alert("The password is too short/long; please try again!")
+    var passLength = parseInt(prompt("Please choose between 8 and 128 characters for your password"));
   }
 
   var passNumber = confirm("Would you like numbers in your password?");
   console.log(passNumber)
-  var passUpper = confirm("Would you like Upper Case characters in your password?");
+  var passUpper = confirm("Would you like upper case characters in your pass word?");
   var passLower = confirm("Would you like lower case characters in your password?");
   var passSpecial = confirm("Would you like special characters in your password?");
 
@@ -33,11 +33,11 @@ function generatePassword() {
     alert("One option must be chosen.  Please pick again!")
     var passNumber = confirm("Would you like numbers in your password?");
     console.log(passNumber)
-    var passUpper = confirm("Would you like Upper Case characters in your password?");
+    var passUpper = confirm("Would you like upper case characters in your password?");
     var passLower = confirm("Would you like lower case characters in your password?");
     var passSpecial = confirm("Would you like special characters in your password?");
   }
-
+  // password arrays
   var arrNumber = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   var arrUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var arrLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
